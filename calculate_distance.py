1dcf4b01b392cdd51ecdea6d380369caedff67db
Manojini51-sys <manojinisibbala@gmail.com>
2025-12-05 21:28:18 +0530

    Add files via upload

---
+++ calculate_distance.py
@@ -0,0 +1,11 @@
+# Program to calculate distance covered by a vehicle 
+# speed and time
+speed = float(input("Enter the speed of the vehicle: "))
+time = float(input("Enter the time traveled: "))
+
+# Formula: Distance = Speed × Time
+distance = speed * time
+
+# distance traveled
+print("Distance traveled by the vehicle:", distance)
+
